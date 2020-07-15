377fa8e859ec9f816e5366d9578ab6ec5c667027
KevinKato123 <k.kato23@viewpoint.org>
2020-07-15 12:07:18 -0700

    LDR

---
+++ Light_Sensor/Light_Sensor.ino
@@ -0,0 +1,18 @@
+
+int ldr_pin = A0; 
+int analogValue = 0;
+
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  analogValue = analogRead(ldr_pin);
+  Serial.println("LDR Value is: "});
+  Serial.println(analogValue);
+  delay(50);
+}
