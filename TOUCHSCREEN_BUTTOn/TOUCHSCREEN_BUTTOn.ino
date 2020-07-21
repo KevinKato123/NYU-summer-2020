f4add02bb306b4fa9fc4554bef79d3fc288695ab
KevinKato123 <k.kato23@viewpoint.org>
2020-07-21 10:51:22 -0700

    Create TOUCHSCREEN_BUTTOn.ino

---
+++ TOUCHSCREEN_BUTTOn/TOUCHSCREEN_BUTTOn.ino
@@ -0,0 +1,53 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_STMPE610.h>
+#include <Adafruit_ILI9341.h>
+//ESP8266
+#define STMPE_CS 16
+#define TFT_CS 0
+#define TFT_DC 15
+#define SD_CS 2
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MAXX 100
+#define TS_MINX 3800
+#define TS_MINY 100
+#define TS_MAXY 3750
+Adafruit_ILI9341 tft = Adafruit_ILI9341 (TFT_CS, TFT_DC);
+Adafruit_STMPE610 ts = Adafruit_STMPE610(STMPE_CS);
+#define BoxSize 40
+void setup() {
+  Serial.begin(115200);
+  if(!ts.begin()){
+    Serial.print("Error : STMPE Error");
+  }
+  tft.begin();
+  tft.fillScreen(ILI9341_BLACK); 
+  tft.fillRect(BoxSize*1,BoxSize*1, BoxSize*4, BoxSize *2, ILI9341_WHITE);
+  tft.setCursor(BoxSize*1.5,BoxSize*1.5); 
+  tft.setTextSize(1);
+  tft.setTextColor(ILI9341_BLACK);
+  tft.println("TOUCH HERE");
+}
+
+void loop() {
+ TS_Point p =ts.getPoint();
+  if(p.x < TS_MINX&&p.x > TS_MAXX && p.y<TS_MAXY && p.y> TS_MINY) {
+    p.x = map(p.x, TS_MINX,TS_MAXX, 0, tft.width());
+    p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+  
+  Serial.print("X = "); Serial.print(p.x);
+  Serial.print("Y = "); Serial.print(p.y);
+  Serial.print("\tPressure = "); Serial.println(p.z);
+
+  if (p.x > BoxSize*1 &&p.x < BoxSize*5 && p.y < BoxSize*1 && p.y > BoxSize *3) {
+    tft.setCursor(BoxSize*1.5, BoxSize*5.5);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_GREEN);
+    Serial.println("DETECTED");
+    tft.println("PRESSED");
+    tft. printf("X: %d | Y: %d", p.x, p.y);
+    delay(1000);
+    yield(); 
+  }
+
+}
+}
